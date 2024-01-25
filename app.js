21dd73d2e6bfc65bf5be0960ca7b093ee95ce49a
Phat07 <nguyenhoangphat852@gmail.com>
2024-01-25 20:10:31 +0700

    merge 2 branch from api/products and api/accounts to master

--- app.js
+++ app.js
@@ -57,13 +57,13 @@ app.use("/accounts", accountRouter);
 app.use("/roles", roleRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
